9e056e34c8982852f79dda957ba4a7091aeeb525
Bruno Gurgel <71857117+Bmg1612@users.noreply.github.com>
2020-09-25 11:04:38 -0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,8 +1,14 @@
 const size = document.querySelector('#sizePicker');
-
+//Wait for the DOM content to be fully loaded
 document.addEventListener('DOMContentLoaded', function () {
+    //Then runs the function
     size.addEventListener('submit', function (e) {
+        /*
+        Prevent the default wich would be
+        sending the form and changing the URL
+        */
         e.preventDefault();
+        //Calls the function
         makeGrid();
     });
 });
@@ -12,26 +18,25 @@ function makeGrid() {
     const gridWidth = document.querySelector('#inputWidth').value;
     const color = document.querySelector('#colorPicker');
     const canvas = document.querySelector('#pixelCanvas');
-    // h because of height
+    /*
+    Resets the canvas and the color of its cells
+    every time the 'submit' button is clicked
+    */ 
     canvas.innerHTML = null;
     canvas.style.backgroundColor = null;
+    /*
+    Looping the number input of the user
+    generating the desirable height and width 
+    */
     for (let h = 0; h < gridHeight; h++) {
         let row = canvas.insertRow(h);
-
-        // w because of width
+        //After the rows are created, then the cells are made
         for (let w = 0; w < gridWidth; w++) {
-            /*
-            I was in doubt about assing this value to a variable, 
-            but i thought maybe it was a good practice.
-            */
             let cell = row.insertCell(w);
         };
 
-    };
-    color.addEventListener('click', function (e) {
-        canvas.style.backgroundColor = color.value;
-    });
-
+    }
+    //Painting the grid that the user clicks(target)
     canvas.addEventListener('click', function (e) {
         e.target.style.backgroundColor = color.value;
     });
