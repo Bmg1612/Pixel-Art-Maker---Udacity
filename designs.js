5b581eed78cbfc9b80c8f71192427eeeb904e670
Bruno Gurgel <71857117+Bmg1612@users.noreply.github.com>
2020-09-26 10:33:25 -0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -14,14 +14,18 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function makeGrid() {
-    const gridHeight = document.querySelector('#inputHeight').value;
-    const gridWidth = document.querySelector('#inputWidth').value;
+    /*
+    Declaring height and width with 'let'
+    because their values keep changing with the user input
+    */
+    let gridHeight = document.querySelector('#inputHeight').value;
+    let gridWidth = document.querySelector('#inputWidth').value;
     const color = document.querySelector('#colorPicker');
     const canvas = document.querySelector('#pixelCanvas');
     /*
     Resets the canvas and the color of its cells
     every time the 'submit' button is clicked
-    */ 
+    */
     canvas.innerHTML = null;
     canvas.style.backgroundColor = null;
     /*
@@ -33,12 +37,18 @@ function makeGrid() {
         //After the rows are created, then the cells are made
         for (let w = 0; w < gridWidth; w++) {
             let cell = row.insertCell(w);
-        };
+        }
 
     }
     //Painting the grid that the user clicks(target)
     canvas.addEventListener('click', function (e) {
-        e.target.style.backgroundColor = color.value;
+        /*
+        Atacching the event listener to the especific cell
+        that the user clicks, not to the whole canvas
+        */
+        if (e.target.nodeName.toLowerCase() === 'td') {
+            e.target.style.backgroundColor = color.value;
+        };
     });
 
 }
